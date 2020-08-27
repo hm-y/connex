df7606d2e444cd16f835671b5add75fe7636fed6
Hilmi Murat Yildirim <hmy0hmy@gmail.com>
2020-08-26 23:16:12 -0400

    Redesign header and footer

---
+++ js/paths.js
@@ -0,0 +1,7 @@
+var path = document.getElementById("curved-path")
+var canvas = path.getContext("2d")
+var width = window.innerWidth()
+var height =window.innerHeight()
+
+canvas.beginPath();
+canvas.moveTo(20 * width / 100, )
