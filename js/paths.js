7ceab131add2f926e795a5e4ed0264fab0eff0f8
Hilmi Murat Yildirim <hmy0hmy@gmail.com>
2020-09-07 18:35:18 -0400

    Modify the main sections

--- js/paths.js
+++
@@ -1,7 +0,0 @@
-var path = document.getElementById("curved-path")
-var canvas = path.getContext("2d")
-var width = window.innerWidth()
-var height =window.innerHeight()
-
-canvas.beginPath();
-canvas.moveTo(20 * width / 100, )
